d77320f296d3f27fda20ae36ad1f4f6990d75e83
Ozehownik <horecki.michal@gmail.com>
2026-09-24 11:45:30 +0200

    Online softmax

--- attn.cu
+++ attn.cu
@@ -1,5 +1,6 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
+#include <cmath>
 
 int main() {
     return 0;
@@ -63,12 +64,14 @@ int main() {
 //     }
 // }
 
-__global__ void flash_attn(const float* Q_global, const float* K_global, int seq_len) {
+__global__ void flash_attn(const float* Q_global, const float* K_global, const float* V_global, int seq_len) {
     // one block takes (M, H) queries and produces T / N of (M, N) patches. So one block computes a (M, T) of scores total. 
     // Later it uses the (M, T) total to compute (M, H) of out.
     __shared__ float Q_shared[M][H];
     __shared__ float K_shared[N][H];
+    __shared__ float V_shared[N][H];
     __shared__ float S_shared[M][N];
+    __shared__ float final_softmax_row_sum[M];
 
     int t_idx = threadIdx.x;
     int m_chunk_idx = blockIdx.x;
@@ -87,9 +90,13 @@ __global__ void flash_attn(const float* Q_global, const float* K_global, int seq
 
     int num_n_chunks = seq_len / N; // you go along T
 
+    // you need the max across the WHOLE (M, T) part.
+    float row_max_old = -INFINITY; 
+    float row_sum_old = 0.0f;
+
     for (int n_chunk = 0; n_chunk < num_n_chunks; n_chunk++) {
 
-        // this block loads (N, H) (K) to shared mem
+        // this block loads K (N, H) to shared mem
         int k_elements_total = N * H;
         for (int i = t_idx; i < k_elements_total; i += NUM_THREADS) {
             int local_row = i / H;
@@ -98,6 +105,17 @@ __global__ void flash_attn(const float* Q_global, const float* K_global, int seq
             int global_row = (n_chunk * N) + local_row;
             K_shared[local_row][local_col] = K_global[global_row * H + local_col];
         }
+
+        int v_elements_total = N * H;
+        for (int i = t_idx; i < v_elements_total; i += NUM_THREADS) {
+            int local_row = i / H;
+            int local_col = i % H;
+
+            int global_row = (n_chunk * N) + local_row;
+            V_shared[local_row][local_col] = V_global[global_row * H + local_col];
+        }
+
+
         __syncthreads();
 
 
@@ -130,15 +148,53 @@ __global__ void flash_attn(const float* Q_global, const float* K_global, int seq
                 }
             }
         }
-        // at ths point S_local has a patch of (M, N)
+        // at ths point S_local has a patch of (THREAD_ROWS, THREAD_COLS)
+
+        float scale = 1.0f / sqrtf(static_cast<float>(H));
 
         for (int i = 0; i < THREAD_ROWS; i++) {
             for (int j = 0; j < THREAD_COLS; j++) {
-                S_shared[row_start + i][col_start + j] = S_local[i][j];
+                S_shared[row_start + i][col_start + j] = S_local[i][j] * scale;
             }
         }
         __syncthreads();
 
         // right now (M, N) is in S_shared
+
+        if (t_idx < M) {
+            // one thread per row looks for MAX
+            int row = t_idx;
+
+            float max_local = -INFINITY;
+            for (int i = 0; i < N; i++) {
+                max_local = fmaxf(max_local, S_shared[row][i]);
+            }
+
+            float row_max_new = fmaxf(row_max_old, max_local);
+            float exp_diff = expf(row_max_old - row_max_new); // e^(old - new) to mul row_sum_old by
+
+            // this loop calculates local sum of exp with new max and writes to S
+            float local_row_sum = 0.0f;
+            for (int i = 0; i < N; i++) {
+                float p = expf(S_shared[row][i] - row_max_new);
+
+                S_shared[row][i] = p;
+
+                local_row_sum += p;
+            }
+
+            // you correct the sum for calculating next tiles on the run (mul old by exp diff)
+            float row_sum_new = row_sum_old * exp_diff + local_row_sum;
+
+            row_max_old = row_max_new;
+            row_sum_old = row_sum_new;
+        }
+
+        // atp (M, N) paths is filled with scores. Now you do matmul to turn (M, T) of S into (M, H) of OUT. V is (N, H)
+        __syncthreads();
+
+        // we have (M, N) in scores, which corresponds to "tokens m, m+1, m+2 pay this much attention to tokens n, n+1, n+2"
+        // Also V of (N, H) means "tokens n, n+1, n+3 have these values"
+        // so naturally you multiply (M, N) x (N, H) = (M, H) = "attention result for m, m+1, m+2 (M) for N first tokens (in V)"
     }
 }
